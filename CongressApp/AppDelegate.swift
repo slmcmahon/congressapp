67db440cf29bf0aebb23a358c3ae0042a96d25e5
Stephen McMahon <stephen@slmcmahon.com>
2016-07-30 13:53:07 +0400

    Initial commit

---
+++ CongressApp/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  CongressApp
+//
+//  Created by Stephen McMahon on 7/30/16.
+//  Copyright © 2016 Stephen McMahon. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
+
---
+++ CongressApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CongressApp
+//
+//  Created by Stephen McMahon on 7/30/16.
+//  Copyright © 2016 Stephen McMahon. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
