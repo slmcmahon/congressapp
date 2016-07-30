7d2614523fa549bffd725651e9c7e6d28d33d6bb
Stephen McMahon <stephen@slmcmahon.com>
2016-07-30 18:31:56 +0400

    More properties for the members view

--- CongressApp/MemberViewController.swift
+++ CongressApp/MemberViewController.swift
@@ -11,10 +11,12 @@ import SwiftyJSON
 
 class MemberViewController : UIViewController {
     
-    @IBOutlet var lblMemberName: UILabel!
-    @IBOutlet var lblMemberPhone: UILabel!
-    @IBOutlet var lblMemberTitle: UILabel!
+    @IBOutlet var lblName: UILabel!
+    @IBOutlet var lblPhone: UILabel!
+    @IBOutlet var lblTitle: UILabel!
     @IBOutlet var lblParty: UILabel!
+    @IBOutlet var lblTwitterId : UILabel!
+    @IBOutlet var lblYoutubeId : UILabel!
     
     var vm : MemberViewModel?
     var memberId : Int?
@@ -23,15 +25,16 @@ class MemberViewController : UIViewController {
         super.viewDidLoad()
         
         vm = MemberViewModel(memberId: memberId, onComplete: memberLoaded, onError: memberLoadFailed)
-        
     }
     
     private func memberLoaded() {
         self.title = vm?.name
-        lblMemberName.text = vm?.name
-        lblMemberPhone.text = vm?.phone
-        lblMemberTitle.text = vm?.title
+        lblName.text = vm?.name
+        lblPhone.text = vm?.phone
+        lblTitle.text = vm?.title
         lblParty.text = vm?.party
+        lblTwitterId.text = vm?.twitterId
+        lblYoutubeId.text = vm?.youtubeId
     }
     
     private func memberLoadFailed(message : String!) {
--- CongressApp/MemberViewModel.swift
+++ CongressApp/MemberViewModel.swift
@@ -16,6 +16,9 @@ public class MemberViewModel {
     var phone : String?
     var party : String?
     var title : String?
+    var webSite : String?
+    var twitterId : String?
+    var youtubeId : String?
     
     public init(memberId : Int!, onComplete:() -> Void, onError:(message : String!) -> Void) {
         Alamofire.request(.GET, "\(Constants.memberBaseUrl)/\(memberId)", parameters: nil)
@@ -33,10 +36,14 @@ public class MemberViewModel {
     private func loadData(data : JSON) {
         
         self.name = "\(data["lastname"].stringValue), \(data["firstname"].stringValue)"
+        self.twitterId = data["twitterid"].stringValue
+        self.youtubeId = data["youtubeid"].stringValue
         let roles = data["roles"].arrayValue.filter { $0["current"] == true }
         let role = roles.last;
         self.phone = role!["phone"].stringValue
         self.party = role!["party"].stringValue
         self.title = role!["role_type_label"].stringValue
+        self.webSite = role!["website"].stringValue
+        
     }
 }
